60864e50f1bb80cdc052f5f03353063682c4cb97
Carl Crozier <carl_crozier@blueyonder.co.uk>
2021-08-05 17:33:46 +0100

    Functional version for single round, commented case detection

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
+    const gameChoices = [
+      "Rock",
+      "Paper",
+      "Scissors"
+    ];
+    
     let playerSelection;
     let computerSelection;
 
-    playerSelection = prompt("Rock, Paper, or Scissors?", "Type Here");
-
-    function computerPlay () {
-        let computerPlayArray = [
-            "Rock",
-            "Paper",
-            "Scissors"
-          ];
-        computerPlay = computerPlayArray[Math.floor(Math.random()*3)];
+    function computerPlay() {
+        return gameChoices[Math.floor(Math.random()*3)];
     }
 
-    computerPlay;
-    console.log(computerPlay);
+    function playRound() {
+      if ((playerSelection == "Rock" && computerSelection == "Paper")
+      || (playerSelection == "Paper" && computerSelection == "Scissors")
+      || (playerSelection == "Scissors" && computerSelection == "Rock")) 
+      {
+        return ("You lose! "+ computerSelection + "beats " + playerSelection);
+      }
+      else if ((playerSelection == "Rock" && computerSelection == "Scissors")
+      || (playerSelection == "Paper" && computerSelection == "Rock")
+      || (playerSelection == "Scissors" && computerSelection == "Rock")) 
+      {
+        return ("You win! "+ playerSelection + " beats " + computerSelection);
+      }
+      else {
+        return ("Draw! " + playerSelection + " is the same as " + computerSelection);
+      }
+    }
+
+    playerSelection = prompt(gameChoices + "?");
+/*    playerSelection.toLowerCase();
+    playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1); */
+    computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    console.log(playRound());
+
+
+    
