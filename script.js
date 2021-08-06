913f7a18570658cc05c4d7bed7839b4dc8485b0f
Carl Crozier <carl_crozier@blueyonder.co.uk>
2021-08-06 11:57:00 +0100

    Added loop function for 5 rounds and a score which is displayed at the end of the game

--- script.js
+++ script.js
@@ -6,6 +6,11 @@
     
     let playerSelection;
     let computerSelection;
+    let playerScore = 0;
+    let computerScore = 0;
+
+    function game() {
+    for (let i = 0; i < 5; i++) { 
 
     function computerPlay() {
         return gameChoices[Math.floor(Math.random()*3)];
@@ -16,18 +21,21 @@
       || (playerSelection3 == "Paper" && computerSelection == "Scissors")
       || (playerSelection3 == "Scissors" && computerSelection == "Rock")) 
       {
+        computerScore = computerScore + 1
         return ("You lose! "+ computerSelection + " beats " + playerSelection3);
       }
       else if ((playerSelection3 == "Rock" && computerSelection == "Scissors")
       || (playerSelection3 == "Paper" && computerSelection == "Rock")
       || (playerSelection3 == "Scissors" && computerSelection == "Rock")) 
       {
+        playerScore = playerScore + 1
         return ("You win! "+ playerSelection3 + " beats " + computerSelection);
       }
       else {
         return ("Draw! " + playerSelection3 + " is the same as " + computerSelection);
       }
     }
+
 
     playerSelection = prompt(gameChoices + "?");
     playerSelection2 = playerSelection.toLowerCase()
@@ -36,5 +44,9 @@
     playRound(playerSelection2, computerSelection);
     console.log(playRound());
 
+  } /* end of the for loop here */
+} /* end of the loop function here */
 
+    game();
+    console.log("Game over! Player: " + playerScore/2 + " | Computer: " + computerScore/2) /* I don't know why but the score needs dividing by 2 to work properly */
     
